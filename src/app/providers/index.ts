33ba49c4823e6da1a2b361f652883310c43a777a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-10 17:29:14 +0300

    feat: add providers for Redux

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,5 +1,6 @@
 import compose from "compose-function";
 
 import { withRouter } from "./with-router";
+import { withStore } from "./with-store";
 
-export const withProviders = compose(withRouter);
+export const withProviders = compose(withStore, withRouter);
---
+++ src/app/providers/with-store.tsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { appStore, persistedStore } from "../appStore";
+
+export const withStore = (component: () => React.ReactNode) => () => (
+  <Provider store={appStore}>
+    <PersistGate loading={null} persistor={persistedStore}>
+      {component()}
+    </PersistGate>
+  </Provider>
+);
