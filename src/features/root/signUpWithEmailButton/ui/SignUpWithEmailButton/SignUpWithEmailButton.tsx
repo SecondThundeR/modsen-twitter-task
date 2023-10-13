28fe468175dda8cda17079d4f3386a6f29d85c01
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-13 15:51:07 +0300

    feat(features): add `useNavigate` for email button

--- src/features/root/signUpWithEmailButton/ui/SignUpWithEmailButton/SignUpWithEmailButton.tsx
+++ src/features/root/signUpWithEmailButton/ui/SignUpWithEmailButton/SignUpWithEmailButton.tsx
@@ -1,8 +1,15 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { RoutePaths } from "@/shared/lib/router";
 import { Button } from "@/shared/ui";
 
 export const SignUpWithEmailButton = memo(function SignUpWithEmailButton() {
-  // TODO: Add Email Sign In feature
-  return <Button text="Sign up with email" />;
+  const navigate = useNavigate();
+
+  const onClick = useCallback(() => {
+    navigate(RoutePaths.register);
+  }, [navigate]);
+
+  return <Button text="Sign up with email" onClick={onClick} />;
 });
