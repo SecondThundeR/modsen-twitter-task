48e5a5da6037f9e542c155ece7403f716225af50
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-13 18:03:17 +0300

    feat(features): add support for Google Sign In

--- src/features/root/signUpWithGoogleButton/ui/SignUpWithGoogleButton/SignUpWithGoogleButton.tsx
+++ src/features/root/signUpWithGoogleButton/ui/SignUpWithGoogleButton/SignUpWithGoogleButton.tsx
@@ -1,9 +1,18 @@
 import { memo } from "react";
 
 import GoogleLogo from "@/shared/assets/googleLogo.svg?react";
+import { useGoogleSignIn } from "@/shared/lib/hooks";
 import { Button } from "@/shared/ui";
 
 export const SignUpWithGoogleButton = memo(function SignUpWithGoogleButton() {
-  // TODO: Add Google Sign In feature
-  return <Button leftSlot={<GoogleLogo />} text="Sign up with Google" />;
+  const { isLoading, onSignIn } = useGoogleSignIn();
+
+  return (
+    <Button
+      leftSlot={<GoogleLogo />}
+      disabled={isLoading}
+      text="Sign up with Google"
+      onClick={onSignIn}
+    />
+  );
 });
