0e27ecad920d093d314e5729f68458e5b5c76abf
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-16 18:29:31 +0300

    feat(shared): add date helpers

---
+++ src/shared/helpers/date.ts
@@ -0,0 +1,9 @@
+export function getDaysAmount(month: number, year: number) {
+  const date = new Date(year, month, 1);
+  const days = [];
+  while (date.getMonth() === month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days.length;
+}
