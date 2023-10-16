4c2edf60b375a13732abec2e4b164c6f1d70a70e
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-16 18:49:21 +0300

    feat(pages): add initial page for signup route

---
+++ src/pages/register/ui/Layout/Layout.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 670px;
+  display: flex;
+  gap: 32px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 16px auto;
+`;
