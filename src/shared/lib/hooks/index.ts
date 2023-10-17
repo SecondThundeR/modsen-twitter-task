89dbc8453ece6798ba9e89d3cdbdf1b3982240bd
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-17 16:15:16 +0300

    feat(shared): add `useDatabase` hook

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useAppDispatch, useAppSelector } from "./store";
 export { useGoogleSignIn } from "./useGoogleSignIn";
 export { useHomeRedirect } from "./useHomeRedirect";
+export { useDatabase } from "./useDatabase/useDatabase";
