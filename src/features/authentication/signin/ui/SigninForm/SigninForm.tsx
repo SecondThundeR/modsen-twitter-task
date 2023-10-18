9d4bf59724b11fc4d58d195dccb836e95616f232
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-18 17:15:23 +0300

    feat(features): initial addition of `SigninForm`
    Note: User can log in only using his email address

---
+++ src/features/authentication/signin/ui/SigninForm/SigninForm.tsx
@@ -0,0 +1,88 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { memo, useState, useCallback } from "react";
+import { useForm } from "react-hook-form";
+
+import { setUser } from "@/entities/user";
+import { useAppDispatch } from "@/shared/lib/hooks";
+import { Alert, Button, Input } from "@/shared/ui";
+import { initiateSignin } from "../../model/signin";
+import {
+  SigninFormSchema,
+  signinFormSchema,
+} from "../../model/signinFormSchema";
+
+import { SigninFormProps } from "./interfaces";
+import { Wrapper } from "./SigninForm.styled";
+
+export const SigninForm = memo(function SigninForm({
+  onComplete,
+}: SigninFormProps) {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    setError,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<SigninFormSchema>({
+    resolver: yupResolver(signinFormSchema),
+    mode: "all",
+  });
+
+  const buttonText = isLoading ? "Entering account..." : "Log in";
+
+  const onSubmit = useCallback(
+    async (params: SigninFormSchema) => {
+      setIsLoading(true);
+
+      try {
+        const res = await initiateSignin(params);
+        const { uid, displayName, email, phoneNumber, dateOfBirth } = res;
+        dispatch(
+          setUser({
+            uid,
+            displayName,
+            email,
+            phoneNumber,
+            dateOfBirth,
+          }),
+        );
+        onComplete?.();
+      } catch (error) {
+        setError("root", {
+          type: "internal",
+          message: (error as Error).message,
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [dispatch, onComplete, setError],
+  );
+
+  return (
+    <Wrapper onSubmit={handleSubmit(onSubmit)}>
+      {errors.root && (
+        <Alert title="Error!" text={errors.root.message} variant="error" />
+      )}
+      <Input
+        type="email"
+        placeholder="Email address"
+        errorMessage={errors.email?.message}
+        {...register("email")}
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        errorMessage={errors.password?.message}
+        {...register("password")}
+      />
+      <Button
+        type="submit"
+        text={buttonText}
+        variant="primary"
+        disabled={isLoading}
+      />
+    </Wrapper>
+  );
+});
---
+++ src/features/authentication/signin/ui/SigninForm/interfaces.ts
@@ -0,0 +1,3 @@
+export interface SigninFormProps {
+  onComplete?: () => void;
+}
