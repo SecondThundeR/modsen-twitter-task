591d25f521c67167a1150330503f98f8bf27ad3c
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-18 14:17:37 +0300

    feat(features): update phone number validation
    Add check for phone number length
    Move out regex and length to own constants
    Update error message when regex check failed

--- src/features/authentication/signup/model/signupFormSchema.ts
+++ src/features/authentication/signup/model/signupFormSchema.ts
@@ -1,13 +1,19 @@
 import { object, string, InferType } from "yup";
 
-const PHONE_NUMBER_REGEX =
-  /^\+?[0-9]{1,3}[-.() ]?(\d{1,3})?[-.() ]?\d{2,4}[-.() ]?\d{2,4}[-.() ]?\d{2,9}$/;
+import {
+  PHONE_NUMBER_REGEX,
+  PHONE_MAX_LENGTH,
+} from "@/shared/constants/validate";
 
 export const signupFormSchema = object({
   name: string().required().min(1, { message: "Name is required" }),
   phoneNumber: string()
     .required()
-    .matches(PHONE_NUMBER_REGEX, "Phone number must have correct format"),
+    .matches(
+      PHONE_NUMBER_REGEX,
+      "Phone number must have correct format, e.g. +1234567890",
+    )
+    .max(PHONE_MAX_LENGTH),
   email: string().required().email({
     message: "Must be a valid email",
   }),
