d5ac536647c93c7612444903759afa1db30ba5f4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-19 21:22:36 +0300

    feat(feature): add `SignoutButton` for sign out ability

---
+++ src/features/authentication/signout/ui/SignoutButton/SignoutButton.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { DISPLAY_MAP, FLEX_PROPERTIES } from "@/shared/constants/generalStyles";
+import { GAP_MAP } from "@/shared/constants/sizing";
+
+export const Wrapper = styled.div`
+  display: ${DISPLAY_MAP.flex};
+  flex-direction: ${FLEX_PROPERTIES.column};
+  gap: ${GAP_MAP.regular};
+`;
