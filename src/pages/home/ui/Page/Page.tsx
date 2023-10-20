a857dcd617621f4ea35ca6a0f3e2e314737ead46
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-20 17:14:05 +0300

    feat(pages): add `Header` for home page

--- src/pages/home/ui/Page/Page.tsx
+++ src/pages/home/ui/Page/Page.tsx
@@ -1,7 +1,15 @@
 import { memo } from "react";
 
+import { ChangeThemeToggle } from "@/features/theme";
+import { Header } from "@/shared/ui";
+
 const Page = memo(function Page() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <Header title="Home" rightSlot={<ChangeThemeToggle />} />
+      <h1>Home page</h1>
+    </>
+  );
 });
 
 export default Page;
