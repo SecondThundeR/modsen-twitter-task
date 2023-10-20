7cb4af5267ee06b0eedd243da99ae3bad65ba3de
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-20 12:43:30 +0300

    fix(shared): add full height for layout

--- src/shared/ui/Layout/Layout.styled.ts
+++ src/shared/ui/Layout/Layout.styled.ts
@@ -5,6 +5,7 @@ import { DISPLAY_MAP, MARGIN_AUTO } from "@/shared/constants/generalStyles";
 import { DIMENSIONS_MAP } from "@/shared/constants/sizing";
 
 export const Wrapper = styled.div`
+  height: ${DIMENSIONS_MAP.fullScreen};
   max-width: ${DIMENSIONS_MAP.narrowLayoutBlock};
   display: ${DISPLAY_MAP.grid};
   grid-template-columns: 1fr 3fr 1fr;
