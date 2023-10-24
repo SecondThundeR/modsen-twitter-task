8282e16fc07f23863ec287ccb38a8aad97e98316
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-24 16:21:10 +0300

    feat(pages): add support for `TweetComposer` in home page

--- src/pages/home/ui/Page/Page.tsx
+++ src/pages/home/ui/Page/Page.tsx
@@ -2,12 +2,14 @@ import { memo } from "react";
 
 import { TweetsList } from "@/widgets/TweetsList/ui/TweetsList/TweetsList";
 import { ChangeThemeToggle } from "@/features/theme";
+import { TweetComposer } from "@/features/tweets/ui/TweetComposer/TweetComposer";
 import { Header } from "@/shared/ui";
 
 const Page = memo(function Page() {
   return (
     <>
       <Header title="Home" rightSlot={<ChangeThemeToggle />} />
+      <TweetComposer />
       <TweetsList />
     </>
   );
