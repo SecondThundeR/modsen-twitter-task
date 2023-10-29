557fcee4c2cef1f0c9046fc22e068ddfa319c38b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-30 00:58:19 +0300

    refactor(app): import `ReactNode` type directly

--- src/app/providers/with-error-boundary.tsx
+++ src/app/providers/with-error-boundary.tsx
@@ -1,5 +1,6 @@
+import type { ReactNode } from "react";
 import { ErrorBoundary } from "@/shared/ui";
 
-export const withErrorBoundary = (component: () => React.ReactNode) => () => (
+export const withErrorBoundary = (component: () => ReactNode) => () => (
   <ErrorBoundary>{component()}</ErrorBoundary>
 );
