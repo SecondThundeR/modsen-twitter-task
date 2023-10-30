bf90a3cb02f1069e3cec3a7d331ea3289d563ac3
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-30 12:53:27 +0300

    feat(widgets): add support for `ProfileImages` in `Sidebar`

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,13 +3,17 @@ import { memo } from "react";
 import { SidebarFooterElements } from "@/widgets/SidebarFooterElements";
 import { SidebarSearch } from "@/widgets/SidebarSearch";
 import { SidebarAuthorsBlock } from "@/features/authors";
+import { ProfileImages, useProfileID } from "@/features/profile";
 
 import { Footer, Wrapper } from "./Sidebar.styled";
 
 export const Sidebar = memo(function Sidebar() {
+  const id = useProfileID();
+
   return (
     <Wrapper>
       <SidebarSearch />
+      {id !== null && <ProfileImages userId={id} />}
       <SidebarAuthorsBlock />
       <Footer>{<SidebarFooterElements />}</Footer>
     </Wrapper>
