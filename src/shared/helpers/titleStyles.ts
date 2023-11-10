777954eb3b878cbe7a554f0d5116f503988af774
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-10 10:47:48 +0300

    refactor(shared): simplify `titleStyles` helpers

--- src/shared/helpers/titleStyles.ts
+++ src/shared/helpers/titleStyles.ts
@@ -1,20 +1,10 @@
 import { FONT_SIZE_MAP } from "@/shared/constants/sizing";
 import { TitleSizes } from "@/shared/ui/Title/interfaces";
 
-const { extrasmall, compact, small, regular, large } = FONT_SIZE_MAP.title;
-
 export function getFontSize(size: TitleSizes) {
-  if (size === "regular") return regular.desktop;
-  if (size === "small") return small.desktop;
-  if (size === "compact") return compact.desktop;
-  if (size === "extrasmall") return extrasmall.desktop;
-  return large.desktop;
+  return FONT_SIZE_MAP.title[size].desktop;
 }
 
 export function getFontSizePhone(size: TitleSizes) {
-  if (size === "regular") return regular.phone;
-  if (size === "small") return small.phone;
-  if (size === "compact") return compact.phone;
-  if (size === "extrasmall") return extrasmall.phone;
-  return large.phone;
+  return FONT_SIZE_MAP.title[size].phone;
 }
